8c156621635f687dc57bb9f331fe75d58c5a2b3f
nunuzy <fnukhi@gmail.com>
2020-10-04 03:50:10 +0700

    first commit

---
+++ app/src/main/java/com/nunu/myflexiblefragment/ProfileActivity.kt
@@ -0,0 +1,10 @@
+package com.nunu.myflexiblefragment
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
